9e99173d33ba336aa8e3c7b123a8b0df4d8cbdc7
Linette Melgara <linettemelgara@Linettes-MacBook-Pro.local>
2025-05-14 21:04:12 -0400

    tambour

--- generate-circle.js
+++ generate-circle.js
@@ -1,3 +1,15 @@
+/**
+ * Parameters are:
+ * 	
+ * 	radius bitRadius depth x y z
+ * 
+ * Call this function using:
+ * 
+ *  node generate-circle.js 30 3 -1 0 0 0.5
+ * 
+ */
+
+
 const STEP_DEPTH = 0.5;
 
 function cutCylinder(radius, bitRadius, depth, x, y, z) {
@@ -16,6 +28,8 @@ function cutSpiral(radius, depth, x, y, z) {
 	for (let i = z; i > depth; i -= STEP_DEPTH) {
 		program += cutG2(radius, x, y, i);
 	}
+	program += cutG2(radius, x, y, depth);
+	program += cutG2(radius, x, y, depth);
 	return `${program}G0 Z${z}\n`;
 }
 
@@ -23,4 +37,4 @@ function cutG2(radius, x, y, z) {
 	return `G2 X${x} Y${y - radius} Z${z} I0 J${radius}\n`;
 }
 
-console.log(cutCylinder(30, 3, -1, 0, 0, 0.5));
+console.log(cutCylinder(Number(process.argv[2]), Number(process.argv[3]), Number(process.argv[4]), Number(process.argv[5]), Number(process.argv[6]), Number(process.argv[7])));
