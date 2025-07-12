201cddc2c8ed117e90d13ce76907114d283590a9
Linette Melgara <linettemelgara@Linettes-MacBook-Pro.local>
2025-07-12 16:57:10 -0400

    cutting board?

--- deepen-pattern.js
+++ deepen-pattern.js
@@ -15,19 +15,36 @@ function deepenPattern(patterns, stepSize, replaceStepSize, depth, start) {
 	return deepenedPattern;
 }
 
-let patterns = [`
-(forward)
-G0 Z-0.5
-G3 X47.7 Y0 Z-0.5 R47.7
-G2 X-47.7 Z-0.5 R47.7
-G3 X-95.4 Y47.7 Z-0.5 R47.7
-`,
-`
-(reverse)
-G0 Z-1.0
-G2 X-47.7 Y0 Z-1.0 R47.7
-G3 X47.7 Y0 Z-1.0 R47.7
-G2 X95.4 Y47.7 Z-1.0 R47.7
-`];
+// let patterns = [`
+// (forward)
+// G0 Z-0.5
+// G3 X47.7 Y0 Z-0.5 R47.7
+// G2 X-47.7 Z-0.5 R47.7
+// G3 X-95.4 Y47.7 Z-0.5 R47.7
+// `,
+// `
+// (reverse)
+// G0 Z-1.0
+// G2 X-47.7 Y0 Z-1.0 R47.7
+// G3 X47.7 Y0 Z-1.0 R47.7
+// G2 X95.4 Y47.7 Z-1.0 R47.7
+// `];
 
-console.log(deepenPattern(patterns, -0.5, -1.0, -5.0, -0.5))
+let patterns = [
+
+`
+(Square Rounded Corners)
+G0 Z-0.5
+G0 X25 Y25 Z-0.5
+G0 X25 Y537.5 Z-0.5
+G2 X37.5 Y550 Z-0.5 R12.5
+G0 X208.5 Y550 Z-0.5
+G2 X220 Y537.5 Z-0.5 R12.5
+G0 X220 Y25 Z-0.5
+G2 X208.5 Y12.5 Z-0.5 R12.5
+G0 X37.5 Y12.5 Z-0.5
+G2 X25 Y25 Z-0.5 R12.5
+`
+];
+
+console.log(deepenPattern(patterns, -0.5, -1.0, -24, -0.5))
