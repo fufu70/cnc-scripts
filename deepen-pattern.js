be961fdd20d3f8d20a2eb807061296cbc925531d
fufu70 <christian.micklisch@icloud.com>
2025-12-28 22:39:38 -0500

    update thumbreader

--- deepen-pattern.js
+++ deepen-pattern.js
@@ -32,22 +32,21 @@ function deepenPattern(patterns, stepSize, replaceStepSize, depth, start) {
 
 let patterns = [
 
-`(Go and cut the outside shape)
-G0 X-15.0 Y5.0 Z0.5 
-G0 Z-0.5
-G2 X15.0 Y5.0 Z-0.5 R17.5
-G3 X17.5 Y3.0 Z-0.5 R3.5
-G0 X47.5 Y3.0 Z-0.5
-G2 X47.5 Y-3.0 Z-0.5 R3.0
-G0 X17.5 Y-3.0 Z-0.5
-G3 X15.0 Y-5.0 Z-0.5 R3.5
-G2 X-15.0 Y-5.0 Z-0.5 R17.5
-G3 X-17.5 Y-3.0 Z-0.5 R3.5
-G0 X-47.5 Y-3.0 Z-0.5
-G2 X-47.5 Y3.0 Z-0.5 R3.0
-G0 X-17.5 Y3.0 Z-0.5
-G3 X-15.0 Y5.0 Z-0.5 R3.5
 `
+G0 X-18.0 Y9.0 Z0.5 
+G0 Z-0.5
+G2 X18.0 Y9.0 Z-0.5 R19
+G3 X19 Y7.8 Z-0.5 R3.5
+G0 X47.5 Y7.8 Z-0.5
+G2 X47.5 Y-7.8 Z-0.5 R7.8
+G0 X19 Y-7.8 Z-0.5
+G3 X18.0 Y-9.0 Z-0.5 R3.5
+G2 X-18.0 Y-9.0 Z-0.5 R19
+G3 X-19 Y-7.8 Z-0.5 R3.5
+G0 X-47.5 Y-7.8 Z-0.5
+G2 X-47.5 Y7.8 Z-0.5 R7.8
+G0 X-19 Y7.8 Z-0.5
+G3 X-18.0 Y9.0 Z-0.5 R3.5`
 ];
 
 console.log(deepenPattern(patterns, -0.5, -1.0, -11.5, -0.5))
